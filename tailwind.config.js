255b53c963262e2215f454cc04085015f890c27c
Vishwak Koleti <koletivishwak84@gmail.com>
2026-01-08 18:27:47 +0530

    Phase 1 Devlopment Done

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto Mono', 'monospace'],
+        pizza: ['Roboto Mono', 'monospace'], // Add this line for the 'font-pizza' class
+      },
+      fontSize: {
+        huge: ['80rem', { lineHeight: '1' }],
+      },
+      height: {
+        screen: '100dvh',
+      },
+    },
+  },
+  plugins: [],
+};
+
